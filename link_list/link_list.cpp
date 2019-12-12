ac052a18d436026d6442d3d242b81bf15dbcd86d
alperyazir <alperyazir66@gmail.com>
2019-12-12 10:57:42 +0300

    Added Reversing  the list with : 1 - iterating 2 - recursive 3 -stack

--- link_list/link_list.cpp
+++ link_list/link_list.cpp
@@ -38,7 +38,7 @@ Node *insert_a_node_to_head(Node *head, int data)
     new_node->next = head;
     return new_node;
 }
-
+/* if index is greater than size of the ll, it adds end of the list */
 Node *insert_a_node_to_index(Node *head, int index, int data)
 {
     Node *new_node = new Node(data);
@@ -48,13 +48,101 @@ Node *insert_a_node_to_index(Node *head, int index, int data)
         return head;
     }
 
+    if (index == 0)
+    {
+        return insert_a_node_to_head(head, data);
+    }
+
     Node *iter = head;
     int counter = 0;
-    while (++counter != index)
+    while (iter->next != nullptr && ++counter != index)
         iter = iter->next;
+
+    if (iter->next == nullptr)
+    {
+        return insert_a_node_to_tail(head, data);
+    }
 
     new_node->next = iter->next;
     iter->next = new_node;
+    return head;
+}
+Node *delete_a_node_to_index(Node *head, int index)
+{
+    // if (head == nullptr)
+    //     return head;
+    // Node *temp = head;
+    // if (index == 0)
+    // {
+    //     head = head->next;
+    //     delete temp;
+    //     return head;
+    // }
+    // int counter{0};
+
+    // while (temp->next != nullptr)
+    // {
+    //     if (++counter == index)
+    //     {
+    //         break;
+    //     }
+    //     temp = temp->next;
+    // }
+
+    // Node *temp1 = temp->next;
+    // temp->next = temp->next->next;
+    // delete temp1;
+    // return head;
+}
+
+Node *reverse_linkedlist_with_recursive(Node *head)
+{
+    if (head == nullptr || head->next == nullptr)
+        return head;
+
+    Node *remainder = reverse_linkedlist_with_recursive(head->next);
+    head->next->next = head;
+    head->next = nullptr;
+    return remainder;
+}
+
+Node *reverse_linkedlist_with_iterative(Node *head)
+{
+    if (head == nullptr)
+        return head;
+
+    Node *prev = nullptr, *current = head, *next;
+
+    while (current != nullptr)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+}
+
+Node *reverse_linkedlist_with_stack(Node *head)
+{
+    if (head == nullptr)
+        return head;
+
+    stack<Node *> m_stack;
+    Node *iter = head;
+    while (iter->next != nullptr)
+    {
+        m_stack.push(iter);
+        iter = iter->next;
+    }
+    head = iter;
+    while (!m_stack.empty())
+    {
+        iter->next = m_stack.top();
+        iter = iter->next;
+        m_stack.pop();
+    }
+    iter->next = nullptr;
     return head;
 }
 
@@ -64,13 +152,25 @@ void print_ll(Node *head)
     if (head == nullptr)
         return;
 
+    while (head != nullptr)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << "\n";
+}
+
+void free_list(Node *head)
+{
+    if (head == nullptr)
+        return;
     Node *iter = head;
     while (iter != nullptr)
     {
-        cout << iter->data << " ";
         iter = iter->next;
+        delete head;
+        head = iter;
     }
-    cout << "\n";
 }
 
 int main()
@@ -84,7 +184,6 @@ int main()
     head = insert_a_node_to_tail(head, 4);
     head = insert_a_node_to_tail(head, 5);
     print_ll(head);
-
     cout << "----------------------------\n";
 
     // Adding nodes to HEad
@@ -97,8 +196,16 @@ int main()
 
     cout << "----------------------------\n";
     // Adding nodes to index( assumed: index starts from 0)
-    head = insert_a_node_to_index(head, 3, 66);
+    head = insert_a_node_to_index(head, 0, 66);
+    head = insert_a_node_to_index(head, 5, 66);
+    head = insert_a_node_to_index(head, 12, 66);
     print_ll(head);
+    cout << "----------------------------\n";
+
+    head = reverse_linkedlist_with_stack(head);
+    print_ll(head);
+    cout << "----------------------------\n";
+    //free_list(head);
 
     return 0;
-} 
+}
