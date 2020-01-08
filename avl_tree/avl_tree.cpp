b89e457cab2f62a6015ecf81137d587449cecab7
alperyazir <alperyazir66@gmail.com>
2020-01-08 10:35:41 +0300

    remove function is added

--- avl_tree/avl_tree.cpp
+++ avl_tree/avl_tree.cpp
@@ -13,6 +13,20 @@ public:
 
 Node *root = NULL;
 
+Node *find_max(Node *root)
+{
+    if (root == nullptr)
+        return root;
+    return root->right == nullptr ? root : find_max(root->right);
+}
+
+Node *find_min(Node *root)
+{
+    if (root == nullptr)
+        return root;
+    return root->left == nullptr ? root : find_min(root->left);
+}
+
 int height_of_node(Node *node)
 {
     if (node == nullptr)
@@ -94,7 +108,9 @@ Node *rl_rotation(Node *node)
 
     return node_right_left;
 }
-
+void balance(Node *n)
+{
+}
 Node *insert(Node *root, int data)
 {
     if (root == nullptr)
@@ -110,13 +126,79 @@ Node *insert(Node *root, int data)
 
     root->height = 1 + max(height_of_node(root->left), height_of_node(root->right));
 
-    if (get_balanced_factor(root) == 2 && get_balanced_factor(root->left) == 1)
+    if (get_balanced_factor(root) > 1 && get_balanced_factor(root->left) >= 0)
         return ll_rotation(root);
-    else if (get_balanced_factor(root) == 2 && get_balanced_factor(root->left) == -1)
+    else if (get_balanced_factor(root) > 1 && get_balanced_factor(root->left) < 0)
         return lr_rotation(root);
-    else if (get_balanced_factor(root) == -2 && get_balanced_factor(root->right) == -1)
+    else if (get_balanced_factor(root) < -1 && get_balanced_factor(root->right) <= 0)
         return rr_rotation(root);
-    else if (get_balanced_factor(root) == -2 && get_balanced_factor(root->right) == 1)
+    else if (get_balanced_factor(root) < -1 && get_balanced_factor(root->right) > 0)
+        return rl_rotation(root);
+
+    return root;
+}
+
+Node *remove(Node *root, int data)
+{
+    if (root == nullptr)
+        return nullptr;
+    if (data < root->data)
+        root->left = remove(root->left, data);
+    else if (data > root->data)
+        root->right = remove(root->right, data);
+    else // Founded Node
+    {
+        // Leaf Node
+        if (root->left == nullptr && root->right == nullptr)
+        {
+            delete root;
+            root = nullptr;
+            return root;
+        }
+        // Has Left Child
+        else if (root->right == nullptr)
+        {
+            Node *temp = root;
+            root = root->left;
+            delete temp;
+            return root;
+        }
+        // Has Right Child
+        else if (root->left == nullptr)
+        {
+            Node *temp = root;
+            root = root->right;
+            delete temp;
+            return root;
+        }
+        // Has Left and Right Child
+        /*
+            Find min of the right or max of right
+        */
+        else
+        {
+            // Find Min of Right Sub Tree
+            // BST *temp = find_min(root->right);
+            // root->m_data = temp->m_data;
+            // root->right = remove(root->right, temp->m_data);
+            // return root;
+
+            // Find Min of Left Sub Tree
+            Node *temp = find_max(root->left);
+            root->data = temp->data;
+            root->left = remove(root->left, temp->data);
+            return root;
+        }
+    }
+    root->height = 1 + max(height_of_node(root->left), height_of_node(root->right));
+
+    if (get_balanced_factor(root) > 1 && get_balanced_factor(root->left) >= 0)
+        return ll_rotation(root);
+    else if (get_balanced_factor(root) > 1 && get_balanced_factor(root->left) < 0)
+        return lr_rotation(root);
+    else if (get_balanced_factor(root) < -1 && get_balanced_factor(root->right) <= 0)
+        return rr_rotation(root);
+    else if (get_balanced_factor(root) < -1 && get_balanced_factor(root->right) > 0)
         return rl_rotation(root);
 
     return root;
@@ -143,9 +225,12 @@ void print_level_order()
 int main()
 {
     root = insert(root, 10);
-    insert(root, 30);
     insert(root, 20);
+    insert(root, 30);
+    insert(root, 40);
+    insert(root, 50);
 
+    remove(root, 10);
     print_level_order();
     return 0;
 }
